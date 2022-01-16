cb7ef5aedb4b2248e95fc888480347169b402abb
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2022-01-16 16:30:22 +0100

    myos -> quickos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/quickos.kernel isodir/boot/quickos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "quickos" {
+	multiboot /boot/quickos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o quickos.iso isodir
